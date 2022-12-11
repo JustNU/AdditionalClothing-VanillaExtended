2e67d9ac6f4b611313ada815d810b5eeb3e1a539
JustNU <ivancool108@gmail.com>
2022-12-11 04:01:51 +0300

    updated bundles

--- src/mod.js
+++ src/mod.js
@@ -12,7 +12,7 @@ class Mod
 		// add tops
 		core.addTop(modDb, "AddClothingVanillaExt_BlackTshirtUpper",	"AddClothingVanillaExt/Tops/top_tshirt_black.bundle", 			"assets/content/hands/hands_tshirt_bear_black/hands_tshirt_bear_black.skin.bundle",	"5d1f5aa286f7744bca3f0b9c");
 		core.addTop(modDb, "AddClothingVanillaExt_FsbAltUpper",			"AddClothingVanillaExt/Tops/top_fsbfastresponse_alt.bundle",	"AddClothingVanillaExt/Hands/hands_fsbfastresponse_alt.bundle",						"5d1f5ab286f7743f8362bcdd");
-		core.addTop(modDb, "AddClothingVanillaExt_FsbAlt2Upper",		"AddClothingVanillaExt/Tops/top_fsbfastresponse_alt2.bundle", 	"AddClothingVanillaExt/Hands/hands_fsbfastresponse_alt2.bundle",					"5d1f5ab286f7743f8362bcdd");
+		core.addTop(modDb, "AddClothingVanillaExt_FsbAlt2Upper",		"AddClothingVanillaExt/Tops/top_fsbfastresponse_alt2.bundle",	"AddClothingVanillaExt/Hands/hands_fsbfastresponse_alt2.bundle",					"5d1f5ab286f7743f8362bcdd");
 		core.addTop(modDb, "AddClothingVanillaExt_Tier2Alt1Upper",		"AddClothingVanillaExt/Tops/top_tier2_alt1.bundle", 			"AddClothingVanillaExt/Hands/hands_tier2_alt1.bundle",								"5e4bb1fe86f774069342dec8");
 		core.addTop(modDb, "AddClothingVanillaExt_Tier2Alt2Upper",		"AddClothingVanillaExt/Tops/top_tier2_alt2.bundle", 			"AddClothingVanillaExt/Hands/hands_tier2_alt2.bundle",								"5e4bb1fe86f774069342dec8");
 		
